84a7243a3b28b9b5195844476aec3ca580c516a7
Mario Lotz <mario@hvboom.ch>
2017-09-05 12:15:45 +0200

    SSL call to HVDigitalSafe raises 'unknown message' error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,11 +37,16 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # FIXME: SSL raises "unknown message" error
+  #
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+  # config.ssl_options = { hsts: { preload: true } }
 end
